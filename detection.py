7d92bf66e7fc9667fbd8b8393f3b13749d9bc09c
ingeborgborhaug <imborhau@stud.ntnu.no>
2025-04-18 11:46:21 +0200

    vol 2

--- detection.py
+++ detection.py
@@ -8,7 +8,9 @@ import cv2
 from datetime import datetime
 
 LOCAL_PATH = "/work/imborhau/football-analysis-detection-and-tracking"
-TEST_IMAGES_PATH = LOCAL_PATH + "/datasets/dataset_3/images"
+TEST_IMAGES_PATH = LOCAL_PATH + "/datasets/dataset_test/images"
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
 IMG_SIZE = 1088
 CONFIDENCE_THRESHOLD = 0.5
@@ -18,7 +20,7 @@ COLORS = {
 }
 
 # model = YOLO("yolov8s.pt")
-model = YOLO('runs/detect/train16/weights/best.pt')
+model = YOLO('runs/detect/train17/weights/best.pt')
 # tracker = DeepSort(max_age=50)
 
 # results = model.train(data= LOCAL_PATH + "/data.yaml", epochs=300, imgsz=IMG_SIZE, dropout=0.3, patience=100, plots=True)
@@ -43,28 +45,24 @@ def draw_detections(frame, detections):
     return frame
 
 # Initialize video writer
-example_image = cv2.imread(LOCAL_PATH + "/datasets/dataset_3/images/000001.jpg")
+example_image = cv2.imread(LOCAL_PATH + "/datasets/dataset_test/images/ds3_000001.jpg")
 VIDEO_HEIGHT, VIDEO_WIDTH, _ = example_image.shape
 
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 output_dir = Path('/work/imborhau/video_outputs')
 output_dir.mkdir(parents=True, exist_ok=True)
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-video_path = output_dir / f"timestamp" / f"output_video_{timestamp}.mp4"
+video_path = output_dir / f"output_video_{timestamp}.mp4"
 video = cv2.VideoWriter(str(video_path), fourcc, 20, (VIDEO_WIDTH, VIDEO_HEIGHT))
-
-# Debug
-print(model.names)
 
 
 for image_path in sorted(Path(TEST_IMAGES_PATH).glob("*.jpg")):
     frame = cv2.imread(str(image_path))
-    frame_resized = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
-    detections = model(frame_resized, verbose=False, conf=0.25)[0]
+    # frame_resized = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+    detections = model(frame, verbose=False, conf=0.25)[0]
     print(f'Number of detections in frame: {len(detections.boxes.data.tolist())}')
 
-    frame_resized_back = cv2.resize(frame_resized, (frame.shape[1], frame.shape[0]))
-    frame_with_detections = draw_detections(frame_resized_back, detections)
+    # frame_resized_back = cv2.resize(frame_resized, (frame.shape[1], frame.shape[0]))
+    frame_with_detections = draw_detections(frame, detections)
     
     video.write(frame_with_detections)
 
